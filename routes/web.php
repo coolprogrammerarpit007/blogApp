5072186c7db292c3a8749322cda58f6d38dc4f75
arpit mishra <arpit.mishra.out@gmail.com>
2024-08-04 08:29:53 +0530

    laravel -11 blog app

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home',function(){
+    return view('home');
+});
+
+// Route::get('/about/{name}',function($name){
+//     // echo $name;
+//     return view('about',['name' => $name]);
+// });
+
+Route::get('/about/{name}',[UserController::class,'getName']);
